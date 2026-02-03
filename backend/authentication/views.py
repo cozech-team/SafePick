f3825c10d9aed14692c726bb1728f5f0e765e550
Ashique <ashique9496@gmail.com>
2026-02-03 14:04:20 +0530

    Final production auth config

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,4 @@
+import os
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
@@ -5,6 +6,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    # This should match your Google Console
-    callback_url = "http://localhost:8000/api/auth/google/callback/"
+    # Use environment variable for production, fallback to localhost for development
+    callback_url = os.environ.get(
+        "GOOGLE_CALLBACK_URL", "http://localhost:8000/api/auth/google/callback/")
     client_class = OAuth2Client
